647e5665c654ff7597423ea1e7a28739869d85e4
nacifyas <nacifyas@gmail.com>
2022-04-18 19:14:59 +0200

    Fixed Alembic integration

--- alembic/env.py
+++ alembic/env.py
@@ -5,9 +5,6 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from sql.database import Base, DATABASE_URL_SYNC
-import sys, os
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -17,15 +14,12 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-    sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-    config.set_main_option('sqlalchemy.url', DATABASE_URL_SYNC)
-
-
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+from sql.sqlmodels import metadata
+target_metadata = metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
