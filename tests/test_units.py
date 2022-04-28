0bec4283a54c2a2e4e908c286d7ed7b8fe9e2339
nacifyas <nacifyas@gmail.com>
2022-04-28 14:19:35 +0200

    Wasdj

--- tests/test_units.py
+++ tests/test_units.py
@@ -1,11 +1,6 @@
-from sqlalchemy import true
 from hub.dal.service import ServiceDAL
 import pytest
 
 
-@pytest.mark.anyo
-async def test_one():
-    # ID: int = 11
-    # service_dal = await ServiceDAL.begin()
-    # assert await service_dal.get_by_id(ID) is not None
+def test_one():
     assert True
