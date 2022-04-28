4732af80c904ec993b3bc49bf4a6afb189168e49
nacifyas <nacifyas@gmail.com>
2022-04-28 03:21:30 +0200

    added windows environment

--- tests/test_units.py
+++ tests/test_units.py
@@ -1,5 +1,11 @@
+from hub.main import app
 from hub.dal.service import ServiceDAL
+from httpx import AsyncClient
+import pytest
 
 
-def test_one():
-    assert True
+@pytest.mark.anyo
+async def test_one():
+    ID: int = 11
+    service_dal = await ServiceDAL.begin()
+    assert await service_dal.get_by_id(ID) is not None
