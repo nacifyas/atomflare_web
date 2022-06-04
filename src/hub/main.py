56e78efb8e1112b2e3af37c66a6357a87350901f
nacifyas <nacifyas@gmail.com>
2022-06-04 23:36:05 +0200

    Implemented better connections (using an actual server) & integrated the createsb script as a dev endpoint

--- src/hub/main.py
+++ src/hub/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from hub.routers import services, users, auths
+from hub.sql.database import engine, Base
 
 app = FastAPI()
 
@@ -9,6 +10,13 @@ app.include_router(users.router)
 app.include_router(auths.router)
 
 #####################
+
+@app.get("/initialize_db")
+async def createdb() -> str:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+        return "database recreated"
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=8000, reload=True, host="127.0.0.1")
--- src/hub/sql/database.py
+++ src/hub/sql/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://fastapi_test:test@127.0.0.1:3306/test"
-DATABASE_URL_SYNC = "postgresql://fastapi_test:test@127.0.0.1:3306/test"
+DATABASE_URL = "postgresql+asyncpg://test_user:password@192.168.0.57:5432/test"
+DATABASE_URL_SYNC = "postgresql://test_user:password@192.168.0.57:5432/test"
 
 engine = create_async_engine(DATABASE_URL, future=True, echo=True)
 async_session = sessionmaker(
